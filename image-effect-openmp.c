f3d783fdf1af82d98bef21bf818f0f13ebf96d82
Ángel M. González <angegonzalez@unal.edu.co>
2022-04-26 14:57:40 -0500

    Resolving tic-toc

--- image-effect-openmp.c
+++ image-effect-openmp.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <omp.h>
+#include <sys/time.h>
 
-// Declare STB_Image library
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -16,33 +16,25 @@
 
 int main(int argc, char *argv[] ) {
     
-    // Validate number of parameters for program execution
-
     if(argc < 4){
     	printf("Usage : <img_in_name> <img_out_name> <number_of_threads>");
     	return -1;
     }
-
-    // Declare filter matrix, information variables for image and threads  
  
     int width, height, bpp;
     int sobel_x[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
     int sobel_y[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
     int number_threads = atoi(argv[3]);
     int thread_id[number_threads];
-
-    // Load image 
+    struct timeval tval_before, tval_after, tval_result;
 
     uint8_t* image = stbi_load(argv[1], &width, &height, &bpp, 1);
     printf("Loaded image with height %d and width %d \n", width, height);
 
-    // Use malloc to reserve memory for ouput image
-
     uint8_t* edge_image;
     edge_image = malloc(width*height);
 
-    // Threads Logic with openMP
-
+    gettimeofday(&tval_before, NULL);
     #pragma omp parallel num_threads(number_threads)
     {
         int thread_id = omp_get_thread_num();
@@ -61,21 +53,54 @@ int main(int argc, char *argv[] ) {
     			      ((sobel_y[2][0]*image[x*height-1+y+1])+ (sobel_y[2][1]* image[x*height+y+1]) + (sobel_y[2][2] * image[x*height+1+y+1]));
     		int val = ceil(sqrt((pixel_x*pixel_x)+(pixel_y*pixel_y)));
     		edge_image[x*height+y] = val;	
-    	    }
-        }
-
+    	}
     }
 
+    }
+    
+    gettimeofday(&tval_after, NULL);
+    timersub(&tval_after, &tval_before, &tval_result);
+    printf("Time elapsed: %ld.%06ld\n", (long int)tval_result.tv_sec, (long int)tval_result.tv_usec);
+
+    // for(int i = 0; i<number_threads; i++){
+    //     thread_id[i] = i;
+    // 	struct threadInfo info = {
+    //         image,
+    // 		edge_image,
+    //         width,
+    // 		height,
+    // 		number_threads,
+    // 		thread_id[i]
+    // 	};   
+    // 	pthread_create(&threads[i], NULL, (void *) calculateFilter, &info);
+    // }
+    
+    // for(int i = 0; i<number_threads; i++){
+    // 	pthread_join(threads[i], NULL);
+    // }
+     
+    // for(int x=1; x< width-2; x++){
+    	
+    // 	for(int y=1; y < height-2; y++){
+    	
+    // 		int pixel_x = ((sobel_x[0][0]*rgb_image[x*height-1+y-1])+ (sobel_x[0][1]* rgb_image[x*height+y-1]) + (sobel_x[0][2] * rgb_image[x*height+1+y-1]))+
+    // 			      ((sobel_x[1][0]*rgb_image[x*height-1+y])+ (sobel_x[1][1]* rgb_image[x*height+y]) + (sobel_x[1][2] * rgb_image[x*height+1+y]))+
+    // 			      ((sobel_x[2][0]*rgb_image[x*height-1+y+1])+ (sobel_x[2][1]* rgb_image[x*height+y+1]) + (sobel_x[2][2] * rgb_image[x*height+1+y+1]));
+    			      
+    // 	        int pixel_y = ((sobel_y[0][0]*rgb_image[x*height-1+y-1])+ (sobel_y[0][1]* rgb_image[x*height+y-1]) + (sobel_y[0][2] * rgb_image[x*height+1+y-1]))+
+    // 			      ((sobel_y[1][0]*rgb_image[x*height-1+y])+ (sobel_y[1][1]* rgb_image[x*height+y]) + (sobel_y[1][2] * rgb_image[x*height+1+y]))+
+    // 			      ((sobel_y[2][0]*rgb_image[x*height-1+y+1])+ (sobel_y[2][1]* rgb_image[x*height+y+1]) + (sobel_y[2][2] * rgb_image[x*height+1+y+1]));
+    			      
+    // 		int val = ceil(sqrt((pixel_x*pixel_x)+(pixel_y*pixel_y)));
+    // 		edge_image[x*height+y] = val;	
+  
+    // 	}
+    
+    // }
     printf("Sobel edge filter was sucessfully applied to %s \n", argv[1]);
     
-    // Memory free for image and size
-
     stbi_image_free(image);    
-    free(edge_image);
-
-    // Write new PNG image
-
     stbi_write_png(argv[2], width, height, CHANNEL_NUM, edge_image, width*CHANNEL_NUM);
-    
     return 0;
 }
+
